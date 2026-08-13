4b0df386e3f968c72783ab8b53d27572adc43658
xinhuey <xinhuey.wong@gmail.com>
2026-08-13 11:27:25 +0800

    getting more pics

--- models/supervised/dataset.py
+++ models/supervised/dataset.py
@@ -43,14 +43,13 @@ IMAGENET_STD = [0.229, 0.224, 0.225]
 # Val/test stays deterministic so evaluation numbers are stable 
 
 train_transforms = transforms.Compose([
-    transforms.Resize((256, 256))
+    transforms.Resize((256, 256)),
     transforms.RandomCrop(IMAGE_SIZE),
-    transforms.RandomHorizontalFlip(p = 0.5),
-    transforms.RandomRotation(degrees = 15),
-    transforms.ColorJitter(brightness=0.2, contrast = 0.2, saturation=0.2),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=15),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
     transforms.ToTensor(),
     transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
-
 ])
 
 eval_transforms = transforms.Compose([
